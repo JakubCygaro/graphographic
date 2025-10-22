eea153823f5485bbdb92fada72ddaf1f5924dd08
papieros <robertrugala@gmail.com>
2025-10-22 06:44:21 +0200

    connection checking

--- graph/graph.go
+++ graph/graph.go
@@ -27,10 +27,34 @@ func New() Graph {
 		Edges: list.New(),
 	}
 }
+
+// Connect two nodes with an edge, get the pointer to the edge
+func (g *Graph) AddEdge(a, b *Node) *Edge {
+	aToB := &Edge{
+		Tail: a,
+		Head: b,
+	}
+	g.Edges.PushBack(aToB)
+	a.Edges.PushBack(aToB)
+	b.Edges.PushBack(aToB)
+	return aToB
+}
+
 func NewNode() Node {
 	return Node {
 		Position: rl.Vector2Zero(),
 		Contents: "",
 		Edges: list.New(),
 	}
+}
+
+func (n *Node) IsConnectedTo(b *Node) bool {
+	for edgeIt := n.Edges.Front(); edgeIt != nil; edgeIt = edgeIt.Next() {
+		edge := edgeIt.Value.(*Edge)
+		if edge.Head == b {
+			return true
+		}
+	}
+	return false
+
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	gr "graphographic/graph"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -8,11 +9,12 @@ import (
 )
 
 const (
-	WIDTH         = 800
-	HEIGHT        = 600
-	FONT_SIZE     = 24
-	FONT_SPACING  = 10
-	SCALE_MINIMUM = 10. / float32(FONT_SIZE)
+	WIDTH          = 800
+	HEIGHT         = 600
+	FONT_SIZE      = 24
+	FONT_SPACING   = 10
+	SCALE_MINIMUM  = 10. / float32(FONT_SIZE)
+	LINE_THICKNESS = 4.0
 )
 const (
 	MODE_PLACE   = iota
@@ -27,9 +29,10 @@ var (
 	Center          rl.Vector2 = rl.Vector2Scale(rl.Vector2{X: WIDTH, Y: HEIGHT}, 0.5)
 	BackgroundColor            = rl.White
 	GraphColor                 = rl.Black
-	Mode            int32      = MODE_APPEND
+	Mode            int32      = MODE_PLACE
 	NodeA           *gr.Node   = nil
 	NodeB           *gr.Node   = nil
+	Directed        bool       = false
 )
 
 func clamp[T constraints.Ordered](value, min, max T) T {
@@ -65,35 +68,38 @@ func main() {
 	rl.CloseWindow()
 }
 
-
-func findNodeUnderMouse(mousePos rl.Vector2) *gr.Node {
-	var ret *gr.Node = nil
-	for nodeIt := Graph.Nodes.Front(); nodeIt != nil; nodeIt = nodeIt.Next() {
-		node := nodeIt.Value.(*gr.Node)
-		radius := rl.MeasureTextEx(rl.GetFontDefault(), node.Contents, float32(FONT_SIZE*Scale), FONT_SPACING).X * 0.5 * Scale
-		if rl.Vector2Distance(node.Position, mousePos) <= radius {
-			return node
-		}
-	}
-	return ret
-}
-
-func getTransformedMousePos() rl.Vector2 {
+// transforms mouse coordinates from screem space to world space
+func getMouseWorldPos() rl.Vector2 {
 	mousePos := rl.GetMousePosition()
 	mousePos = rl.Vector2Subtract(mousePos, Center)
 	mousePos = rl.Vector2Subtract(mousePos, Offset)
 	mousePos = rl.Vector2Scale(mousePos, 1/Scale)
 	return mousePos
 }
-func getTransformedPos(position rl.Vector2) rl.Vector2 {
+
+// transforms coordinates from world space to screen space
+func getScreenPos(position rl.Vector2) rl.Vector2 {
 	position = rl.Vector2Scale(position, Scale)
 	position = rl.Vector2Add(position, Center)
 	position = rl.Vector2Add(position, Offset)
 	return position
 }
 
+// mousePos must be in screen space, node positions will be transformed into screen space
+func findNodeUnderMouse(mousePos rl.Vector2) *gr.Node {
+	var ret *gr.Node = nil
+	for nodeIt := Graph.Nodes.Front(); nodeIt != nil; nodeIt = nodeIt.Next() {
+		node := nodeIt.Value.(*gr.Node)
+		radius := rl.MeasureTextEx(rl.GetFontDefault(), node.Contents, float32(FONT_SIZE*Scale), FONT_SPACING).X * 0.5 * Scale
+		if rl.Vector2Distance(getScreenPos(node.Position), mousePos) <= radius + LINE_THICKNESS {
+			return node
+		}
+	}
+	return ret
+}
+
 func update() {
-	mousePos := getTransformedMousePos()
+	mousePos := getMouseWorldPos()
 	if rl.IsKeyReleased(rl.KeyC) {
 		Mode = wrap(Mode+1, int32(MODE_PLACE), int32(MODE_CONNECT))
 	}
@@ -102,7 +108,18 @@ func update() {
 		switch Mode {
 		case MODE_CONNECT:
 			if NodeA == nil {
-				NodeA = findNodeUnderMouse(mousePos)
+				NodeA = findNodeUnderMouse(rl.GetMousePosition())
+			}
+		case MODE_APPEND:
+			if NodeA == nil {
+				NodeA = findNodeUnderMouse(rl.GetMousePosition())
+			}
+			if NodeA != nil && NodeB == nil {
+				NodeB = &gr.Node{
+					Position: NodeA.Position,
+					Contents: "New Node",
+					Edges: list.New(),
+				}
 			}
 		}
 	}
@@ -117,22 +134,25 @@ func update() {
 				&node,
 			)
 		case MODE_CONNECT:
-			NodeB = findNodeUnderMouse(mousePos)
-			if NodeA != nil && NodeB != nil {
-				Graph.Edges.PushBack(&gr.Edge {
-					Tail: NodeA,
-					Head: NodeB,
-				})
-				Graph.Edges.PushBack(&gr.Edge {
-					Head: NodeA,
-					Tail: NodeB,
-				})
+			NodeB = findNodeUnderMouse(rl.GetMousePosition())
+			if NodeA != nil && NodeB != nil && !NodeA.IsConnectedTo(NodeB){
+				Graph.AddEdge(NodeA, NodeB)
+				if !Directed {
+					Graph.AddEdge(NodeB, NodeA)
+				}
 			}
-			NodeA = nil
-			NodeB = nil
+		case MODE_APPEND:
+			if NodeA != nil && NodeB != nil {
+				Graph.Nodes.PushBack(NodeB)
+				Graph.AddEdge(NodeA, NodeB)
+				if !Directed {
+					Graph.AddEdge(NodeB, NodeA)
+				}
+			}
 		}
+		NodeA = nil
+		NodeB = nil
 	}
-
 
 	if rl.IsMouseButtonDown(rl.MouseButtonRight) {
 		delta := rl.GetMouseDelta()
@@ -147,6 +167,16 @@ func update() {
 }
 
 func draw() {
+	if NodeA != nil {
+		mousePos := rl.GetMousePosition()
+		position := getScreenPos(NodeA.Position)
+		rl.DrawLineEx(
+			mousePos,
+			position,
+			LINE_THICKNESS,
+			GraphColor,
+		)
+	}
 	drawGraph()
 
 	var mode string = "Mode: "
@@ -167,39 +197,28 @@ func draw() {
 		FONT_SPACING,
 		rl.Red,
 	)
-	if NodeA != nil {
-		mousePos := rl.GetMousePosition()
-		position := getTransformedPos(NodeA.Position)
-		rl.DrawLine(
-			int32(position.X),
-			int32(position.Y),
-			int32(mousePos.X),
-			int32(mousePos.Y),
-			rl.Black,
-		)
-	}
 }
 
-
 func drawGraph() {
+	// draw edges
 	for edgeIt := Graph.Edges.Front(); edgeIt != nil; edgeIt = edgeIt.Next() {
 		edge := edgeIt.Value.(*gr.Edge)
-		tailPos := getTransformedPos(edge.Tail.Position)
-		headPos := getTransformedPos(edge.Head.Position)
-		rl.DrawLine(
-			int32(tailPos.X),
-			int32(tailPos.Y),
-			int32(headPos.X),
-			int32(headPos.Y),
-			rl.Black,
+		tailPos := getScreenPos(edge.Tail.Position)
+		headPos := getScreenPos(edge.Head.Position)
+		rl.DrawLineEx(
+			tailPos,
+			headPos,
+			LINE_THICKNESS,
+			GraphColor,
 		)
 	}
+	// draw nodes
 	for nodeIt := Graph.Nodes.Front(); nodeIt != nil; nodeIt = nodeIt.Next() {
 		node := nodeIt.Value.(*gr.Node)
 		radius := rl.MeasureTextEx(rl.GetFontDefault(), node.Contents, float32(FONT_SIZE*Scale), FONT_SPACING).X * 0.5 * Scale
-		position := getTransformedPos(node.Position)
+		position := getScreenPos(node.Position)
 		textPosition := position
-		rl.DrawCircle(int32(position.X), int32(position.Y), radius+3.0, GraphColor)
+		rl.DrawCircle(int32(position.X), int32(position.Y), radius+LINE_THICKNESS, GraphColor)
 		rl.DrawCircle(int32(position.X), int32(position.Y), radius, BackgroundColor)
 
 		size := rl.MeasureTextEx(rl.GetFontDefault(), node.Contents, FONT_SIZE*Scale, FONT_SPACING)
